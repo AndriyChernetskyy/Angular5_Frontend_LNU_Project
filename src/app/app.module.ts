26111fa9fb69ebe9fb43819fdbbb04013e5452df
andrew179 <andrewusa921@gmail.com>
2018-05-28 18:19:30 +0300

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstBlockComponent } from './first-block/first-block.component';
+import { ServicesComponent } from './services/services.component';
+import { WorkComponent } from './work/work.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TeamComponent } from './team/team.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstBlockComponent,
+    ServicesComponent,
+    WorkComponent,
+    SkillsComponent,
+    TeamComponent,
+    TestimonialComponent,
+    AboutComponent,
+    
+    
   ],
   imports: [
     BrowserModule
